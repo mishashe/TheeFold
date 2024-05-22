76c60f203c4ecf558a40a43fd11e6030b9619146
Florian Massip <florian.massip@gmail.com>
2024-05-22 11:55:21 +0200

    adapt misha files to CBIO cluster

--- src/TwoDirsFindMatches.R
+++ src/TwoDirsFindMatches.R
@@ -1,9 +1,10 @@
 args = commandArgs(trailingOnly=TRUE)
-species1 <- args[1]
-species2 <- args[2]
+sp1 <- args[1]
+sp2 <- args[2]
 
 # dir <- "/home/misha/Documents/Development/ThreeFold/"
-dir <- "/scratch/ws1/msheinman-msheinman/ThreeFold/"
+#dir <- "/scratch/ws1/msheinman-msheinman/ThreeFold/"
+dir <- "/cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/multi_comparisons/"
 # sp1 <- "Escherichia_coli"
 # sp2 <- "Klebsiella_pneumoniae"
 # sp2 <- "Salmonella_enterica"
@@ -18,7 +19,7 @@ if (!file.exists(paste0(dir,"data/external/",sp1,".fna"))) {system(paste0("cat "
 if (!file.exists(paste0(dir,"data/external/",sp2,".fna"))) {system(paste0("cat ",dir,"data/external/",sp2,"/*.fna > ",dir,"data/external/",sp2,".fna"))}
 
 # find matches between two files
-soft <- "/home/misha/Documents/Development/Software/bfmem/bfmem "
+soft <- "/cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/multi_comparisons/refseq_filter_download/bfmem/bfmem "
 # soft <- "/home/misha/Documents/Development/Software/copmem2-main/copmem2 "
 system(paste0("mkdir -p ",dir,"data/processed/",sp2,"_vs_",sp1,"/"))
 Command <- paste0(soft," -s b -t 128 -l 300  -o ",dir,"data/processed/",sp2,"_vs_",sp1,"/",sp2,"_vs_",sp1,".tsv -r ",dir,"data/external/",sp1,".fna"," -q ",dir,"data/external/",sp2,".fna")
