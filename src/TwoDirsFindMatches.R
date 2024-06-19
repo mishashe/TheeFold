9258a9441b260af678ce309aa3e89e2ca022919a
Florian Massip <florian.massip@gmail.com>
2024-06-19 12:45:58 +0200

    deprecated scripts

--- src/TwoDirsFindMatches.R
+++ src/TwoDirsFindMatches.R
@@ -11,19 +11,17 @@ dir <- "/cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/multi_comparisons/"
 
 
 # remove plasmids headers, concatenate with "n" break
-if (!file.exists(paste0(dir,"data/external/",sp1,".fna"))) {system(paste0("sed -i -r '1 ! s/^(>[^ ]+) .*/n/' ",dir,"data/external/",sp1,"/*.fna"))}
-if (!file.exists(paste0(dir,"data/external/",sp2,".fna"))) {system(paste0("sed -i -r '1 ! s/^(>[^ ]+) .*/n/' ",dir,"data/external/",sp2,"/*.fna"))}
-
+#if (!file.exists(paste0(dir,"data/external/fasta/",sp1,".fna"))) {system(paste0("sed -i -r '1 ! s/^(>[^ ]+) .*/n/' ",dir,"data/external/fasta/",sp1,"/*.fna"))}
+#if (!file.exists(paste0(dir,"data/external/fasta/",sp2,".fna"))) {system(paste0("sed -i -r '1 ! s/^(>[^ ]+) .*/n/' ",dir,"data/external/fasta/",sp2,"/*.fna"))}
+#
 # concatenate all files to one
-if (!file.exists(paste0(dir,"data/external/",sp1,".fna"))) {system(paste0("cat ",dir,"data/external/",sp1,"/*.fna > ",dir,"data/external/",sp1,".fna"))}
-if (!file.exists(paste0(dir,"data/external/",sp2,".fna"))) {system(paste0("cat ",dir,"data/external/",sp2,"/*.fna > ",dir,"data/external/",sp2,".fna"))}
+#if (!file.exists(paste0(dir,"data/external/fasta/",sp1,".fna"))) {system(paste0("cat ",dir,"data/external/fasta/",sp1,"/*.fna > ",dir,"data/external/fasta/",sp1,".fna"))}
+#if (!file.exists(paste0(dir,"data/external/fasta/",sp2,".fna"))) {system(paste0("cat ",dir,"data/external/fasta/",sp2,"/*.fna > ",dir,"data/external/fasta/",sp2,".fna"))}
 
 # find matches between two files
-soft <- "/cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/multi_comparisons/refseq_filter_download/bfmem/bfmem "
+soft <- "/cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/multi_comparisons/ThreeFold/src/bfmem/bfmem "
 # soft <- "/home/misha/Documents/Development/Software/copmem2-main/copmem2 "
-system(paste0("mkdir -p ",dir,"data/processed/",sp2,"_vs_",sp1,"/"))
-Command <- paste0(soft," -s b -t 128 -l 300  -o ",dir,"data/processed/",sp2,"_vs_",sp1,"/",sp2,"_vs_",sp1,".tsv -r ",dir,"data/external/",sp1,".fna"," -q ",dir,"data/external/",sp2,".fna")
+system(paste0("mkdir -p ",dir,"data/processed/",sp2,"_",sp1,"/"))
+Command <- paste0(soft," -s b -t 1 -l 300  -o ",dir,"data/processed/pairs/",sp2,"_",sp1,".tsv -r ",dir,"data/external/fasta/",sp1,".fna"," -q ",dir,"data/external/fasta/",sp2,".fna")
 system(Command)
-
-
 
