107d55ea6808ffdd1a28f1164b2381e5e5f6ba93
Florian Massip <florian.massip@gmail.com>
2024-09-05 11:43:10 +0200

    add pbd option

--- src/mobilome/extract_mobilome.py
+++ src/mobilome/extract_mobilome.py
@@ -27,7 +27,11 @@ parser.add_option("-o","--output_file",dest="output_file",action="store",
 help="output file name")
 parser.add_option("-d", "--db_file",dest="db_file",action="store",
 help="file containing the database of genes in the mobilome")
-parser.add_option("-n", "--nb_strains",dest="nb_strains",action="store",type = int, default = 20,
+
+parser.add_option("-p", "--pdb_file",dest="pdb_file",action="store",default = "NO",
+help="file containing an other database of genes in the mobilome, but this time with protein AA sequence instead of nucleotides")
+
+parser.add_option("-n", "--nb_strains",dest="nb_strains",action="store",type = int, default = 50,
 help="the number of strains from which we want to extract the mobilome")
 
 
@@ -38,6 +42,7 @@ pattATCG = re.compile("[^ATCGNatcgn\n]")
 #query_file = options.query_file
 clade_dir = options.clade_dir
 db_file = options.db_file
+pdb_file = options.pdb_file
 output_file=options.output_file
 nb_strains = options.nb_strains
 pattNewline = re.compile("\\n")
@@ -47,37 +52,52 @@ pattNewline = re.compile("\\n")
 
 
 
-def launch_blast(db_file,query_file,clade_dir):
+def launch_blast(db_file,pdb_file,subject_file,clade_dir):
 
 	Matches = {}	
 	soft = "/cluster/CBIO/home/fmassip/mambaforge/envs/HGT/bin/blastn"
-	query_full = clade_dir + "/"+ query_file
-	print(query_full)
-	print('blastn','-subject',db_file,'-query',query_full)
-	res_blast = subprocess.run(['blastn','-subject',db_file,'-query',query_full,'-outfmt','6'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+	subject_full = clade_dir + "/"+ subject_file
+	print(subject_full)
+	print('blastn','-subject',subject_full,'-query',db_file)
+	os.system("date")
+	res_blast = subprocess.run(['blastn','-subject',subject_full,'-query',db_file,'-outfmt','6'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+	os.system("date")
 #	f1.write(res_blast+"\n")
-	for line in pattNewline.split(res_blast):
+#	if db_file:
+#		print("yes test")
+	print('tblastn','-subject',subject_full,'-query',pdb_file)
+	if pdb_file != "NO":
+		res_tblast = subprocess.run(['tblastn','-subject',subject_full,'-query',pdb_file,'-outfmt','6'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+		os.system("date")
+		all_res = res_blast+res_tblast
+	else:
+		all_res = res_blast
+		print("no pdb")
+
+	for line in pattNewline.split(all_res):
 		row = line.split('\t')
 		
 		if len(row)>2 :
-			row[7] = int(row[7])
-			row[6] = int(row[6])
-			if row[7] > row[6]:
-				start = row[6]
-				end = row[7]
+			row[8] = int(row[8])
+			row[9] = int(row[9])
+			if row[8] > row[9]:
+				start = max(0,row[9]-500)
+				end = row[8]+500
 			else:
-				start = row[7]
-				end = row[6]
+				start = max(0,row[8]-500)
+				end = row[9]+500
  
-			if row[0] in Matches.keys():
-				Matches[row[0]].append([start,end])
+			if row[1] in Matches.keys():
+				Matches[row[1]].append([start,end])
 			else:
-				Matches[row[0]]=[[start,end]]
+				Matches[row[1]]=[[start,end]]
+
+
 	return(Matches)
 
 
 
-def mergeOverlap_and_extract_fasta(Matches,query_file,opFile,clade_dir): 
+def mergeOverlap_and_extract_fasta(Matches,subject_file,opFile,clade_dir): 
 	os.system("mkdir -p " + output_file + "_tmp")
 	for cle in Matches.keys():
 		all_pos = np.array(Matches[cle])
@@ -86,10 +106,10 @@ def mergeOverlap_and_extract_fasta(Matches,query_file,opFile,clade_dir):
 		chrs = np.ones(all_pos_sorted.shape[0],dtype = 'object')
 		chrs[:] = cle
 		final_array = np.vstack((chrs,all_pos_sorted.transpose().astype('object'))).transpose()
-		np.savetxt(output_file+'_tmp/'+query_file,final_array,delimiter = "\t",fmt = '%s')
+		np.savetxt(output_file+'_tmp/'+subject_file,final_array,delimiter = "\t",fmt = '%s')
 		
 
-	concatenated_dat = subprocess.run(['mergeBed','-i',output_file+'_tmp/'+query_file,'-d','100'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+	concatenated_dat = subprocess.run(['mergeBed','-i',output_file+'_tmp/'+subject_file,'-d','1000'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 	toExtract = {}
 	concatenated_dat = concatenated_dat.rstrip()
 
@@ -100,16 +120,17 @@ def mergeOverlap_and_extract_fasta(Matches,query_file,opFile,clade_dir):
 		else:
 			toExtract[row[0]]=[[row[1],row[2]]]
 		
-	query_full = clade_dir + "/"+ query_file
-	fasta_data = open(query_full,"r")
-	records = list(SeqIO.parse(query_full, "fasta"))
-	all_contigs = SeqIO.to_dict(SeqIO.parse(query_full, "fasta"))
+	subject_full = clade_dir + "/"+ subject_file
+	fasta_data = open(subject_full,"r")
+	records = list(SeqIO.parse(subject_full, "fasta"))
+	all_contigs = SeqIO.to_dict(SeqIO.parse(subject_full, "fasta"))
 
 	for cle in toExtract.keys():
 		opFile.write(">"+cle+"\n")
 		for index in range(0,len(toExtract[cle])):
+			contig_len = len(all_contigs[cle])
 			start = int(toExtract[cle][index][0])
-			end = int(toExtract[cle][index][1])
+			end = min(int(toExtract[cle][index][1]),contig_len-1)
 			subseq = str(all_contigs[cle].seq[start:end])
 			subseqN = re.sub(pattATCG,"N",subseq) #convert Ambiguous caracters to Ns
 			opFile.write(subseqN)
@@ -130,11 +151,19 @@ f2.close()
 
 f1= open(output_file+'_seq.fa',"w")
 
+if pdb_file != "NO":
+	print("GO")
+else:
+	print("yes I can pbd file")
+	print (pdb_file)
+
+
 for myFile in randList:
 	print(myFile)
-	Matches = launch_blast(db_file = db_file,query_file = myFile,clade_dir = clade_dir)
+	print (pdb_file)
+	Matches = launch_blast(db_file = db_file,pdb_file = pdb_file ,subject_file = myFile,clade_dir = clade_dir)
 	if Matches:
-		mergeOverlap_and_extract_fasta(Matches = Matches, query_file = myFile ,opFile = f1,clade_dir = clade_dir)
+		mergeOverlap_and_extract_fasta(Matches = Matches, subject_file = myFile ,opFile = f1,clade_dir = clade_dir)
 
 
 
